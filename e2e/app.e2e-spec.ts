6a642dd55ff1d7fce42eb3dba1e3efade874b01a
Angular CLI <angular-cli@angular.io>
2017-08-05 15:59:17 -0600

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { BudgetAppPage } from './app.po';
+
+describe('budget-app App', () => {
+  let page: BudgetAppPage;
+
+  beforeEach(() => {
+    page = new BudgetAppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
